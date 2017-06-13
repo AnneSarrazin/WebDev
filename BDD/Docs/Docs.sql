06409abdf35ac91522b84e86cce0bf05f4957839
Anne <anne.sarrazin@isen.yncrea.fr>
2017-06-13 13:52:18 +0200

    Rajout url

--- BDD/Docs/Docs.sql
+++ BDD/Docs/Docs.sql
@@ -8,7 +8,8 @@
 #------------------------------------------------------------
 
 CREATE TABLE Documents(
-        iDoc          int (11) Auto_increment  NOT NULL ,
+        idDoc          int (11) Auto_increment  NOT NULL ,
+        url            Varchar (250) NOT NULL UNIQUE ,
         nomDoc         Varchar (250) NOT NULL ,
         descriptionDoc Varchar (400) ,
         PRIMARY KEY (idDoc )
