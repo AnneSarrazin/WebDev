f627df1d36b15cbe54d3d13bf40ae15a8bd06fcc
Anne <anne.sarrazin@isen.yncrea.fr>
2017-06-14 12:39:56 +0200

    Ajout du changement de locataire operationnel

--- Reservation/ModificationReservationLoc.php
+++ Reservation/ModificationReservationLoc.php
@@ -3,7 +3,9 @@ $Locataire=$_POST['locataire'];
 $DateDeb=$_POST['saveDateDeb'];
 $DateFin=$_POST['saveDateFin'];
 
-require '../BDD/Resas/Objet_Resa/resa.php';
+require 'resa.php';
+
+echo $Locataire;
 
 $dbh = new PDO('mysql:host=localhost;dbname=Mandeline_Resa', 'root','');
 $Resa= new resa($dbh,$DateDeb,$DateFin,NULL,0);
