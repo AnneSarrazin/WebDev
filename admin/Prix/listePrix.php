9278219dcd1c0f0de550158802439bdf51336b2b
Anne <anne.sarrazin@isen.yncrea.fr>
2017-06-14 18:36:25 +0200

    Debut affichage liste tarif

---
+++ admin/Prix/listePrix.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <table>
+            <tr><th>Date de début</th><th>Date de fin</th><th>Prix</th></tr>
+        <?php
+        $dbh = new PDO('mysql:host=localhost;dbname=Mandeline_Resa', 'root','');
+        foreach($dbh->query('SELECT * FROM prix') as $row){
+            echo "<tr><td>".$row['dateDeb']."</td><td>".$row['dateFin']."</td><td>".$row['tarif']."</td></tr>";
+        }
+        ?>
+        </table>
+    </body>
+</html>
