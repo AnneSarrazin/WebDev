3c1cc503cdf47aeb9f3214d886ca2de2f76e9fe6
Anne <anne.sarrazin@isen.yncrea.fr>
2017-06-14 18:51:16 +0200

    Ajout Suppression

--- admin/Prix/SupprimePrix.php
+++ admin/Prix/SupprimePrix.php
@@ -4,3 +4,8 @@ $DateFin=$_GET['dateFin'];
 $Tarif=$_GET['Tarif'];
 
 
+require './Prix.php';
+
+$dbh = new PDO('mysql:host=localhost;dbname=Mandeline_Resa', 'root','');
+$MonTarif=new Prix($dbh, $DateDeb, $DateFin, $Tarif);
+$MonTarif->destruction($dbh);
--- admin/Prix/listePrix.php
+++ admin/Prix/listePrix.php
@@ -7,11 +7,11 @@
     </head>
     <body>
         <table>
-            <tr><th>Date de début</th><th>Date de fin</th><th>Prix</th></tr>
+            <tr><th>Date de début</th><th>Date de fin</th><th>Prix</th><th>Supprimer</th></tr>
         <?php
         $dbh = new PDO('mysql:host=localhost;dbname=Mandeline_Resa', 'root','');
         foreach($dbh->query('SELECT * FROM prix') as $row){
-            echo "<tr><td>".$row['dateDeb']."</td><td>".$row['dateFin']."</td><td>".$row['Tarif']."€</td></tr>";
+            echo "<tr><td>".$row['dateDeb']."</td><td>".$row['dateFin']."</td><td>".$row['Tarif']."€</td><td><a href=\"SupprimePrix.php?dateDeb=".$row['dateDeb']."&dateFin=".$row['dateFin']."&Tarif=".$row['Tarif']."\">Supprimer</a></td></tr>";
         }
         ?>
         </table>
