8bf4230c8dfa9fd59b42edde511ee0544c27b0e3
LMD <luca.mayer-dalverny@isen.yncrea.fr>
2017-06-15 14:00:29 +0200

    MàJ Header
    mise à jour des deux header du site

--- Header/Header.php
+++ Header/Header.php
@@ -5,20 +5,20 @@ $urlSite = 'http://localhost/Mandeline';
 <link href="../Style/Header.css" rel="stylesheet" type="text/css"/>
 
 <header>
+    <h1>La Mandeline Locations</h1>
     <img id="Banniere" src="../Header/PANO_20160816_165610.jpg" alt="Banniere du site"/>
-        <h1>La Mandeline Locations</h1>
-        <div>
-            <a href="#Navigation"><img src="../Header/mandeline.png" alt=""/></a>
-            <nav id="Navigation" >
-                <ul>
-                    <?php
-                    echo '<li><a href="'.$urlSite.'/Accueil.php" >Accueil</a></li>';
-                    echo '<li><a href="pages/Reservations.php">Disponibilités</a></li>';
-                    echo '<li><a href="pages/GaleriePhoto.php">Galerie Photo</a></li>';
-                    echo '<li><a href="pages/Contact.php">Contact</a></li>';
-                    echo '<li id="Fermer" ><a href="#">Fermer</a></li>';
-                    ?>    
-                </ul>
-            </nav>
-        </div>
+    <div>
+        <a href="#Navigation"><img src="../Header/mandeline.png" alt=""/></a>
+        <nav id="Navigation" >
+            <ul>
+                <?php
+                echo '<li><a href="'.$urlSite.'/Accueil.php" >Accueil</a></li>';
+                echo '<li><a href="pages/Reservations.php">Disponibilités</a></li>';
+                echo '<li><a href="pages/GaleriePhoto.php">Galerie Photo</a></li>';
+                echo '<li><a href="pages/Contact.php">Contact</a></li>';
+                echo '<li id="Fermer" ><a href="#">Fermer</a></li>';
+                ?>    
+            </ul>
+        </nav>
+    </div>
 </header>
--- Header/HeaderAdmin.php
+++ Header/HeaderAdmin.php
@@ -5,23 +5,23 @@ $urlSite = 'http://localhost/Mandeline';
 <link href="../Style/Header.css" rel="stylesheet" type="text/css"/>
 
 <header>
+    <h1>La Mandeline Locations</h1>
     <img id="Banniere" src="../Header/PANO_20160816_165610.jpg" alt="Banniere du site"/>
-        <h1>La Mandeline Locations</h1>
-        <div>
-            <a href="#Navigation"><img src="../Header/mandeline.png" alt=""/></a>
-            <nav id="Navigation" >
-                <ul>
-                    <?php
-                    echo '<li><a href="'.$urlSite.'/Accueil.php" >Accueil</a></li>';
-                    echo '<li><a href="">Réservations</a></li>';
-                    echo '<li><a href="">Locataires</a></li>';
-                    echo '<li><a href="">Commentaires</a></li>';
-                    echo '<li><a href="">Photos</a></li>';
-                    echo '<li><a href="">Texte d\'accueil</a></li>';
-                    echo '<li><a href="">Documents</a></li>';
-                    echo '<li id="Fermer" ><a href="#">Fermer</a></li>';
-                    ?>    
-                </ul>
-            </nav>
-        </div>
+    <div>
+        <a href="#Navigation"><img src="../Header/mandeline.png" alt=""/></a>
+        <nav id="Navigation" >
+            <ul>
+                <?php
+                echo '<li><a href="'.$urlSite.'/Accueil.php" >Accueil</a></li>';
+                echo '<li><a href="">Réservations</a></li>';
+                echo '<li><a href="../ListeLocataire/ListeLocataire.php">Locataires</a></li>';
+                echo '<li><a href="../ListeCommentaire/ListeCommentaire.php">Commentaires</a></li>';
+                echo '<li><a href="">Photos</a></li>';
+                echo '<li><a href="">Texte d\'accueil</a></li>';
+                echo '<li><a href="">Documents</a></li>';
+                echo '<li id="Fermer" ><a href="#">Fermer</a></li>';
+                ?>    
+            </ul>
+        </nav>
+    </div>
 </header>
