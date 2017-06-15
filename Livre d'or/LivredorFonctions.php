0fab38e5d84ce9ac44024095deee5a98c9740764
onceuponaban <onceuponaban@gmail.com>
2017-06-15 18:42:12 +0200

    Correction d'un bug dans le livre d'or

--- Livre d'or/LivredorFonctions.php
+++ Livre d'or/LivredorFonctions.php
@@ -5,11 +5,11 @@ function afficheCommentaire($dbh, $Tab, $i)
     $stmt->execute(); 
     $NomPrenom = $stmt->fetch();
     echo '<p><strong>Auteur :</strong> '.$NomPrenom[0].' '.$NomPrenom[1].'</p>';
+    if(isset($Tab[$i][3])) //Si le commentaire a une réservation associée
+    {
     $stmt = $dbh->prepare('SELECT dateDeb, dateFin FROM reservation WHERE idResa = '.$Tab[$i][3]); //On récupère les dates de réservations (si possible)
     $stmt->execute(); 
     $DatesDebutFin = $stmt->fetch();
-    if(isset($DatesDebutFin))
-    {
         echo '<p>Période de réservation du '.$DatesDebutFin[0].' au '.$DatesDebutFin[1].'</p>';
     }
     echo '<p><strong>Contenu du commentaire :</strong> '.$Tab[$i][1].'</p>';
