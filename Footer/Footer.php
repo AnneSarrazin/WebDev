121d195a16f80cc7e377cc279defd1f083af3075
LMD <luca.mayer-dalverny@isen.yncrea.fr>
2017-06-15 16:51:08 +0200

    Footer du Site

---
+++ Footer/Footer.php
@@ -0,0 +1,10 @@
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<link href="../Style/Footer.css" rel="stylesheet" type="text/css"/>
+
+<footer>
+    <p id="promotion">Site réalisé par des étudiants de l'<a href="https://isen-toulon.fr/" >ISEN Toulon</a>   <a id="admin" href="" >Partie Admin</a></p>
+</footer>
